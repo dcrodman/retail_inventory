fdfd8d701e293893fe217f1b8d19fae1ebe4c024
Drew Rodman <dcrodman@gmail.com>
2013-11-24 18:53:15 -0500

    Remove unneeded base url routing.

--- urls.py
+++ urls.py
@@ -1,7 +1,3 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.base import RedirectView
 
-from dashboard import views
-
-urlpatterns = patterns('',
-)
+urlpatterns = patterns('',)
