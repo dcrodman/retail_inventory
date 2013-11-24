3ca8608b2b376352a55143529a76c12c96bd67e1
Drew Rodman <dcrodman@gmail.com>
2013-11-24 18:53:37 -0500

    Add URL routing for views in modules.

--- RetailInventory/url_names.py
+++ RetailInventory/url_names.py
@@ -1,4 +1,33 @@
 """Module with constants for URL names used for page redirects for each
 application view."""
+from django.core.urlresolvers import reverse
 
 DASHBOARD = 'dashboard'
+# Stock Module URLS
+STOCK_OVERVIEW = 'stock_overview'
+PRODUCT_SEARCH = 'product_search'
+# Employee Module URLS
+ADD_EMPLOYEE = 'add_employee'
+EMPLOYEE_DETAILS = 'employee_details'
+SEARCH_EMPLOYEE = 'search_employee'
+
+
+# Shortcut URLS for resolving reverse matches.
+
+def dashboard_url():
+  return reverse(DASHBOARD)
+
+def stock_overview_url():
+  return reverse(STOCK_OVERVIEW)
+
+def product_search_url():
+  return reverse(PRODUCT_SEARCH)
+
+def add_employee_url():
+  return reverse(ADD_EMPLOYEE)
+
+def employee_details_url():
+  return reverse(EMPLOYEE_DETAILS)
+
+def search_employee_url():
+  return reverse(SEARCH_EMPLOYEE)
--- RetailInventory/urls.py
+++ RetailInventory/urls.py
@@ -1,12 +1,23 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from django.views.generic.base import RedirectView
+from django.views.generic.simple import redirect_to
 
 from dashboard import views
+from employees import urls as employee_urls
+from stock import urls as stock_urls
+from transactions import urls as transaction_urls
+
 import url_names
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='/dashboard')),
+    url(r'^$', redirect_to, {'url' : '/dashboard'}),
     url(r'^dashboard/$', views.DashboardView.as_view(), 
-        name=url_names.DASHBOARD)
+        name=url_names.DASHBOARD),
+
+    url(r'^stock/', include(stock_urls)),
+    url(r'^employees/', include(employee_urls)),
+    #url(r'^transactions/', include(transaction_urls)),
+  
+    #url(r'^customers/', include(stock_urls)),
+    #url(r'^scheduling/', include(stock_urls)),
+    #url(r'^store/', include(stock_urls))
 )
