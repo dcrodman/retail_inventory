db459274d18c98e891045edb0d6081d991991eb1
Drew Rodman <dcrodman@gmail.com>
2013-11-24 18:53:01 -0500

    Update view to dynamically provide all URLs to user.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from RetailInventory.url_names import *
+
 class DashboardView(View):
   """Class defining the main dashboard view."""
   
   def get(self, request):
-    return render(request, 'dashboard.html')
+    context = {
+        'title' : 'Dashboard',
+        'stock_overview' : stock_overview_url(),
+        'product_search' : product_search_url(),
+        'add_employee' : add_employee_url(),
+        'employee_details' : employee_details_url(),
+        'search_employees' : search_employee_url()
+      }
+    return render(request, 'dashboard.html', context)
